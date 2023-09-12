f320c694db2e500edddd5c123315cb41cae79e8b
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2023-09-12 23:06:54 +0200

    Add project files.

---
+++ CarWorkskop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarWorkskop.MVC.Models;
+
+namespace CarWorkskop.MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        var aboutModel = new List<About>()
+        {
+            new About()
+            {
+                Title = "Nowy tytuł",
+                Description = "Nowy opis",
+                Tags = new List<string> {"tag1", "tag2"}
+            },
+            new About()
+            {
+                Title = "Nowy tytuł2",
+                Description = "Nowy opis2",
+                Tags = new List<string> {"tag3", "tag4"}
+
+            },
+        };
+        return View(aboutModel);
+    }
+
+    public IActionResult Contact()
+    {
+        var contactModel = new Contact()
+        {
+            Phone = 1234456,
+            Email = "myEmail@wp.pl"
+        };
+        return View(contactModel);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ CarWorkskop.MVC/Models/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace CarWorkskop.MVC.Models
+{
+    public class Contact
+    {
+        public int Phone {  get; set; }
+        public string Email { get; set; }
+    }
+}
